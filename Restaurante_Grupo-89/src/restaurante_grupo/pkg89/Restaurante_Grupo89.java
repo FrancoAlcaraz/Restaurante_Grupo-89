8cb8a5af65383fa4adad2de91e076db48e2be044
FrancoAlcaraz <franco231000@gmail.com>
2023-10-02 11:48:12 -0300

    UML

---
+++ Restaurante_Grupo-89/src/restaurante_grupo/pkg89/Restaurante_Grupo89.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurante_grupo.pkg89;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Restaurante_Grupo89 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

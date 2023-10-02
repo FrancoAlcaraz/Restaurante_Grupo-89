69cdb2ef3cadad0aaa554ca7c6260be85ae5af70
BelLeal7 <bel7leal@gmail.com>
2023-10-02 12:58:20 -0300

    clases entidades

--- Restaurante_Grupo-89/src/AccesoADatos/Producto.java
+++ Restaurante_Grupo-89/src/AccesoADatos/Producto.java
@@ -1,7 +1,69 @@
-
 package AccesoADatos;
 
-
 public class Producto {
+
+    private int idProducto;
+    private String nombre;
+    private int cantidad;
+    private double precio;
+    private Categoria categoria;
+
+    public Producto(int idProducto, String nombre, int cantidad, double precio, Categoria categoria) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+
+    public Producto() {
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "idProducto=" + idProducto + ", nombre=" + nombre + ", cantidad=" + cantidad + ", precio=" + precio + ", categoria=" + categoria + '}';
+    }
+
+    
     
 }
--- Restaurante_Grupo-89/src/AccesoADatos/Reserva.java
+++ Restaurante_Grupo-89/src/AccesoADatos/Reserva.java
@@ -1,7 +1,83 @@
 
 package AccesoADatos;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 
 class Reserva {
+    private int idReserva;
+    private int dni;
+    private String nombre;
+    private LocalDate fecha;
+    private LocalTime hora;
+    private boolean estado;
+
+    public Reserva(int idReserva, int dni, String nombre, LocalDate fecha, LocalTime hora, boolean estado) {
+        this.idReserva = idReserva;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.fecha = fecha;
+        this.hora = hora;
+        this.estado = estado;
+    }
+
+    public Reserva() {
+    }
+
+    public int getIdReserva() {
+        return idReserva;
+    }
+
+    public void setIdReserva(int idReserva) {
+        this.idReserva = idReserva;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public LocalTime getHora() {
+        return hora;
+    }
+
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Reserva{" + "idReserva=" + idReserva + ", dni=" + dni + ", nombre=" + nombre + ", fecha=" + fecha + ", hora=" + hora + ", estado=" + estado + '}';
+    }
+    
+    
     
 }

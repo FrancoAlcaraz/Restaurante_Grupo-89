7fe8549f4721a71106796a8ddf819ac115af950f
Mari0130 <ltev.electronica@gmail.com>
2023-10-03 08:44:04 -0300

    Clase Entidades

--- Restaurante_Grupo-89/src/Entidades/Mesero.java
+++ Restaurante_Grupo-89/src/Entidades/Mesero.java
@@ -3,5 +3,76 @@ package Entidades;
 
 
 public class Mesero {
+    int IdMesero;
+    int dni;
+    String nombre;
+    boolean Estado;
+    Mesa mesa;
+    Reserva reserva;
+
+    public Mesero(int IdMesero, int dni, String nombre, boolean Estado, Mesa mesa, Reserva reserva) {
+        this.IdMesero = IdMesero;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.Estado = Estado;
+        this.mesa = mesa;
+        this.reserva = reserva;
+    }
+
+    public Mesero() {
+    }
+
+    public int getIdMesero() {
+        return IdMesero;
+    }
+
+    public void setIdMesero(int IdMesero) {
+        this.IdMesero = IdMesero;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isEstado() {
+        return Estado;
+    }
+
+    public void setEstado(boolean Estado) {
+        this.Estado = Estado;
+    }
+
+    public Mesa getMesa() {
+        return mesa;
+    }
+
+    public void setMesa(Mesa mesa) {
+        this.mesa = mesa;
+    }
+
+    public Reserva getReserva() {
+        return reserva;
+    }
+
+    public void setReserva(Reserva reserva) {
+        this.reserva = reserva;
+    }
+
+    @Override
+    public String toString() {
+        return "Mesero{" + "IdMesero=" + IdMesero + ", dni=" + dni + ", nombre=" + nombre + ", Estado=" + Estado + ", mesa=" + mesa + ", reserva=" + reserva + '}';
+    }
     
 }
--- Restaurante_Grupo-89/src/Entidades/Pedidos.java
+++ Restaurante_Grupo-89/src/Entidades/Pedidos.java
@@ -3,5 +3,56 @@ package Entidades;
 
 
 public class Pedidos {
+    int IdPedido;
+    Producto producto;
+    Mesero mesero;
+    Mesa mesa;
+
+    public Pedidos(int IdPedido, Producto producto, Mesero mesero, Mesa mesa) {
+        this.IdPedido = IdPedido;
+        this.producto = producto;
+        this.mesero = mesero;
+        this.mesa = mesa;
+    }
+
+    public Pedidos() {
+    }
+
+    public int getIdPedido() {
+        return IdPedido;
+    }
+
+    public void setIdPedido(int IdPedido) {
+        this.IdPedido = IdPedido;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Mesero getMesero() {
+        return mesero;
+    }
+
+    public void setMesero(Mesero mesero) {
+        this.mesero = mesero;
+    }
+
+    public Mesa getMesa() {
+        return mesa;
+    }
+
+    public void setMesa(Mesa mesa) {
+        this.mesa = mesa;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedidos{" + "IdPedido=" + IdPedido + ", producto=" + producto + ", mesero=" + mesero + ", mesa=" + mesa + '}';
+    }
     
 }

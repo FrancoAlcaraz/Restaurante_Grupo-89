358f0e74bdf28753e95423c44ddb8ddc16b10af4
FrancoAlcaraz <franco231000@gmail.com>
2023-10-06 11:50:48 -0300

    menu

--- Restaurante_Grupo-89/src/Vistas/Menu.java
+++ Restaurante_Grupo-89/src/Vistas/Menu.java
@@ -27,70 +27,110 @@ public class Menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jDesktopPane1 = new javax.swing.JDesktopPane();
+        jEscritorio = new javax.swing.JDesktopPane();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
-        jMenuItem2 = new javax.swing.JMenuItem();
-        jMenuItem4 = new javax.swing.JMenuItem();
-        jMenuItem5 = new javax.swing.JMenuItem();
-        jMenuItem3 = new javax.swing.JMenuItem();
-        jMenuItem1 = new javax.swing.JMenuItem();
+        AgregarPedido = new javax.swing.JMenuItem();
+        AgregarProducto = new javax.swing.JMenuItem();
+        AgregarMesa = new javax.swing.JMenuItem();
+        AgregarMesero = new javax.swing.JMenuItem();
+        AgregarReserva = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
-        jMenuItem6 = new javax.swing.JMenuItem();
-        jMenuItem8 = new javax.swing.JMenuItem();
-        jMenuItem9 = new javax.swing.JMenuItem();
-        jMenuItem7 = new javax.swing.JMenuItem();
+        PrecioProducto = new javax.swing.JMenuItem();
+        PedidosXMesa = new javax.swing.JMenuItem();
+        pedidosXMesero = new javax.swing.JMenuItem();
+        productoXCategoria = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
-        jDesktopPane1.setLayout(jDesktopPane1Layout);
-        jDesktopPane1Layout.setHorizontalGroup(
-            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout jEscritorioLayout = new javax.swing.GroupLayout(jEscritorio);
+        jEscritorio.setLayout(jEscritorioLayout);
+        jEscritorioLayout.setHorizontalGroup(
+            jEscritorioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 400, Short.MAX_VALUE)
         );
-        jDesktopPane1Layout.setVerticalGroup(
-            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        jEscritorioLayout.setVerticalGroup(
+            jEscritorioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 279, Short.MAX_VALUE)
         );
 
         jMenu1.setText("Nuevos");
 
-        jMenuItem2.setText("Agregar Pedido");
-        jMenuItem2.addActionListener(new java.awt.event.ActionListener() {
+        AgregarPedido.setText("Agregar Pedido");
+        AgregarPedido.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenuItem2ActionPerformed(evt);
+                AgregarPedidoActionPerformed(evt);
             }
         });
-        jMenu1.add(jMenuItem2);
+        jMenu1.add(AgregarPedido);
 
-        jMenuItem4.setText("Agregar Producto");
-        jMenu1.add(jMenuItem4);
+        AgregarProducto.setText("Agregar Producto");
+        AgregarProducto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AgregarProductoActionPerformed(evt);
+            }
+        });
+        jMenu1.add(AgregarProducto);
 
-        jMenuItem5.setText("Agregar Mesa");
-        jMenu1.add(jMenuItem5);
+        AgregarMesa.setText("Agregar Mesa");
+        AgregarMesa.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AgregarMesaActionPerformed(evt);
+            }
+        });
+        jMenu1.add(AgregarMesa);
 
-        jMenuItem3.setText("Agregar Mesero");
-        jMenu1.add(jMenuItem3);
+        AgregarMesero.setText("Agregar Mesero");
+        AgregarMesero.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AgregarMeseroActionPerformed(evt);
+            }
+        });
+        jMenu1.add(AgregarMesero);
 
-        jMenuItem1.setText("Agregar Reserva");
-        jMenu1.add(jMenuItem1);
+        AgregarReserva.setText("Agregar Reserva");
+        AgregarReserva.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AgregarReservaActionPerformed(evt);
+            }
+        });
+        jMenu1.add(AgregarReserva);
 
         jMenuBar1.add(jMenu1);
 
         jMenu2.setText("Consultas");
 
-        jMenuItem6.setText("Precios de Productos");
-        jMenu2.add(jMenuItem6);
+        PrecioProducto.setText("Precios de Productos");
+        PrecioProducto.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                PrecioProductoActionPerformed(evt);
+            }
+        });
+        jMenu2.add(PrecioProducto);
 
-        jMenuItem8.setText("Pedidos por Mesa");
-        jMenu2.add(jMenuItem8);
+        PedidosXMesa.setText("Pedidos por Mesa");
+        PedidosXMesa.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                PedidosXMesaActionPerformed(evt);
+            }
+        });
+        jMenu2.add(PedidosXMesa);
 
-        jMenuItem9.setText("Pedidos por Mesero");
-        jMenu2.add(jMenuItem9);
+        pedidosXMesero.setText("Pedidos por Mesero");
+        pedidosXMesero.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                pedidosXMeseroActionPerformed(evt);
+            }
+        });
+        jMenu2.add(pedidosXMesero);
 
-        jMenuItem7.setText("Productos por Categoria");
-        jMenu2.add(jMenuItem7);
+        productoXCategoria.setText("Productos por Categoria");
+        productoXCategoria.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                productoXCategoriaActionPerformed(evt);
+            }
+        });
+        jMenu2.add(productoXCategoria);
 
         jMenuBar1.add(jMenu2);
 
@@ -100,23 +140,101 @@ public class Menu extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPane1)
+            .addComponent(jEscritorio)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPane1)
+            .addComponent(jEscritorio)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jMenuItem2ActionPerformed
+    private void AgregarPedidoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AgregarPedidoActionPerformed
+         jEscritorio.removeAll();
+      jEscritorio.repaint();
+      AgregarPedido ap=new AgregarPedido();
+      ap.setVisible(true);
+      jEscritorio.add(ap);
+      jEscritorio.moveToFront(ap);
+    }//GEN-LAST:event_AgregarPedidoActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
+    private void AgregarProductoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AgregarProductoActionPerformed
+       jEscritorio.removeAll();
+      jEscritorio.repaint();
+      AgregarProducto apr=new AgregarProducto();
+      apr.setVisible(true);
+      jEscritorio.add(apr);
+      jEscritorio.moveToFront(apr);
+    }//GEN-LAST:event_AgregarProductoActionPerformed
+
+    private void AgregarMesaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AgregarMesaActionPerformed
+
+      jEscritorio.removeAll();
+      jEscritorio.repaint();
+     AgregarMesa am=new AgregarMesa();
+      am.setVisible(true);
+      jEscritorio.add(am);
+      jEscritorio.moveToFront(am);
+    }//GEN-LAST:event_AgregarMesaActionPerformed
+
+    private void AgregarMeseroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AgregarMeseroActionPerformed
+      jEscritorio.removeAll();
+      jEscritorio.repaint();
+      AgregarMesero ame=new AgregarMesero();
+      ame.setVisible(true);
+      jEscritorio.add(ame);
+      jEscritorio.moveToFront(ame);
+    }//GEN-LAST:event_AgregarMeseroActionPerformed
+
+    private void AgregarReservaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AgregarReservaActionPerformed
+
+      jEscritorio.removeAll();
+      jEscritorio.repaint();
+      AgregarReserva ar=new AgregarReserva();
+      ar.setVisible(true);
+      jEscritorio.add(ar);
+      jEscritorio.moveToFront(ar);
+    }//GEN-LAST:event_AgregarReservaActionPerformed
+
+    private void PrecioProductoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PrecioProductoActionPerformed
+      jEscritorio.removeAll();
+      jEscritorio.repaint();
+      PreciosDeProducto pp=new PreciosDeProducto();
+      pp.setVisible(true);
+      jEscritorio.add(pp);
+      jEscritorio.moveToFront(pp);
+        
+    }//GEN-LAST:event_PrecioProductoActionPerformed
+
+    private void PedidosXMesaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PedidosXMesaActionPerformed
+     jEscritorio.removeAll();
+     jEscritorio.repaint();
+      PedidosPorMesa ppm=new PedidosPorMesa();
+      ppm.setVisible(true);
+      jEscritorio.add(ppm);
+      jEscritorio.moveToFront(ppm);        
+    }//GEN-LAST:event_PedidosXMesaActionPerformed
+
+    private void pedidosXMeseroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_pedidosXMeseroActionPerformed
+    jEscritorio.removeAll();
+    jEscritorio.repaint();
+    PedidosPorMesero ppme=new PedidosPorMesero();
+    ppme.setVisible(true);
+    jEscritorio.add(ppme);
+    jEscritorio.moveToFront(ppme);
+    }//GEN-LAST:event_pedidosXMeseroActionPerformed
+
+    private void productoXCategoriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_productoXCategoriaActionPerformed
+    jEscritorio.removeAll();
+    jEscritorio.repaint();
+    ProductosPorCategoria ppc=new ProductosPorCategoria();
+      ppc.setVisible(true);
+    jEscritorio.add(ppc);
+    jEscritorio.moveToFront(ppc);
+    }//GEN-LAST:event_productoXCategoriaActionPerformed
+
+   
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
@@ -150,18 +268,18 @@ public class Menu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JMenuItem AgregarMesa;
+    private javax.swing.JMenuItem AgregarMesero;
+    private javax.swing.JMenuItem AgregarPedido;
+    private javax.swing.JMenuItem AgregarProducto;
+    private javax.swing.JMenuItem AgregarReserva;
+    private javax.swing.JMenuItem PedidosXMesa;
+    private javax.swing.JMenuItem PrecioProducto;
+    private javax.swing.JDesktopPane jEscritorio;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JMenuItem jMenuItem2;
-    private javax.swing.JMenuItem jMenuItem3;
-    private javax.swing.JMenuItem jMenuItem4;
-    private javax.swing.JMenuItem jMenuItem5;
-    private javax.swing.JMenuItem jMenuItem6;
-    private javax.swing.JMenuItem jMenuItem7;
-    private javax.swing.JMenuItem jMenuItem8;
-    private javax.swing.JMenuItem jMenuItem9;
+    private javax.swing.JMenuItem pedidosXMesero;
+    private javax.swing.JMenuItem productoXCategoria;
     // End of variables declaration//GEN-END:variables
 }
